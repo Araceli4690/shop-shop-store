8fc00eaef7ec6faf608f715040f1500dad7e899c
araceli <carmona.araceli.4690@gmail.com>
2021-09-20 16:09:25 -0700

    components updated to use redux useSelector and useDispatch

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,13 +2,17 @@ import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
-import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import spinner from '../../assets/spinner.gif';
 import { idbPromise } from '../../utils/helpers';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state
+  })
+
+  const dispatch = useDispatch();
 
   const { currentCategory } = state;
 
