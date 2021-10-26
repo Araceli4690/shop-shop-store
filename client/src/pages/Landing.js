9e12b103a845c7422e257847b558634c85a96fb0
araceli <carmona.araceli.4690@gmail.com>
2021-10-25 22:12:43 -0700

    added another iamge to landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,26 +2,50 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Cart from "../components/Cart";
 import shopImg from "../assets/shop.png"
+import shopImg2 from "../assets/shop2.png"
 
 const Landing = () => {
     return (
-        <section style={{ display: 'flex' }}>
+        <section >
             <Cart />
-            <div className='intro'>
+            <div className='landing' style={{ display: 'flex' }}>
+                <div className='intro'>
 
-                <div>
-                    Your one stop shop destination for all your needs. Shop our store and view all the great deals!
+                    <div>
+                        Your one stop shop destination for all your needs!
+                    </div>
+                    <br />
+                    <div className="button-container">
+
+                        <Link to="/Home">
+                            <button className="homeButton">Shop Now</button>
+                        </Link>
+                    </div>
                 </div>
-                <div className="button-container">
 
-                    <Link to="/Home">
-                        <button className="homeButton">Shop Now</button>
-                    </Link>
+                <div className='landingImg'>
+                    <img src={shopImg} alt='landing'></img>
                 </div>
             </div>
+            <div style={{ display: 'flex' }}>
+                <div className='landingImg'>
+                    <img src={shopImg2} alt='landing'></img>
+                </div>
+                <div className='intro'>
 
-            <div className='landingImg'>
-                <img src={shopImg} alt='landing'></img>
+                    <div>
+                        Your one stop shop destination for all your needs!
+                    </div>
+                    <br />
+                    <div className="button-container">
+
+                        <Link to="/Home">
+                            <button className="homeButton">Shop Now</button>
+                        </Link>
+                    </div>
+                </div>
+
+
             </div>
         </section>
     )
